3a112a7c49a6962d1dcfdbcbd3acdf3a645f3c1e
TheNisham <77533996+nishamghimire5@users.noreply.github.com>
2023-09-25 23:52:32 +0545

    initial ui changed

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,6 +1,8 @@
 import 'package:camera/camera.dart';
-import 'package:flutter/material.dart';
 import 'package:digital_humanities/camera_page.dart';
+import 'package:flutter/material.dart';
+import 'package:circular_bottom_navigation/circular_bottom_navigation.dart';
+import 'package:circular_bottom_navigation/tab_item.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -10,20 +12,218 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int selectedPos = 0;
+
+  double bottomNavBarHeight = 60;
+
+  List<TabItem> tabItems = List.of([
+    TabItem(
+      Icons.home,
+      "Home",
+      Colors.blue,
+      labelStyle: const TextStyle(
+        fontWeight: FontWeight.normal,
+      ),
+    ),
+    TabItem(
+      Icons.search,
+      "Search",
+      Colors.orange,
+      labelStyle: const TextStyle(
+        color: Colors.red,
+        fontWeight: FontWeight.bold,
+      ),
+    ),
+    TabItem(
+      Icons.history,
+      "Recents",
+      Colors.red,
+      circleStrokeColor: Colors.black,
+    ),
+    TabItem(
+      Icons.camera,
+      "Capture",
+      Colors.cyan,
+    ),
+  ]);
+
+  late CircularBottomNavigationController _navigationController;
+
+  @override
+  void initState() {
+    super.initState();
+    _navigationController = CircularBottomNavigationController(selectedPos);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Scan")),
-      body: SafeArea(
-        child: Center(
-            child: ElevatedButton(
-          onPressed: () async {
-            await availableCameras().then((value) => Navigator.push(context,
-                MaterialPageRoute(builder: (_) => CameraPage(cameras: value))));
-          },
-          child: const Text("Take a Picture"),
-        )),
+      body: Stack(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.only(bottom: bottomNavBarHeight),
+            child: bodyContainer(),
+          ),
+          Align(alignment: Alignment.bottomCenter, child: bottomNav())
+        ],
       ),
     );
+  }
+
+  Widget bodyContainer() {
+    List<String> files = [
+      'Nisham',
+      'ShreeGanesh',
+      'Saswat',
+      'Angelina',
+      'Nilotpal',
+    ];
+    if (selectedPos == 3) {
+      return FutureBuilder<List<CameraDescription>>(
+        future: availableCameras(),
+        builder: (BuildContext context,
+            AsyncSnapshot<List<CameraDescription>> snapshot) {
+          if (snapshot.hasData) {
+            return CameraPage(cameras: snapshot.data!);
+          } else {
+            return const Center(child: CircularProgressIndicator());
+          }
+        },
+      );
+    }
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.bottomLeft,
+          end: Alignment.topRight,
+          colors: [
+            Colors.lightBlue[100]!,
+            Colors.purple[200]!,
+            Colors.lightBlue[100]!,
+          ],
+        ),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 50),
+            child: Row(
+              children: [
+                const Padding(
+                  padding: EdgeInsets.only(left: 16),
+                  child: Text(
+                    'View Your',
+                    style: TextStyle(
+                      color: Colors.black87,
+                      fontSize: 19,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                const Spacer(),
+                IconButton(
+                  onPressed: () {
+                    // handle settings button press
+                  },
+                  icon: const Icon(Icons.settings_outlined),
+                ),
+              ],
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.only(left: 16, bottom: 22),
+            child: Text(
+              'Recents',
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+                color: Colors.blue,
+              ),
+            ),
+          ),
+          // Add your recents widget here
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: TextField(
+              decoration: InputDecoration(
+                hintText: '    Search here...',
+                suffixIcon: const Icon(
+                  Icons.search,
+                  color: Colors.blue,
+                ),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20),
+                  // borderSide: BorderSide.none,
+                ),
+              ),
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.only(left: 16, bottom: 22, top: 22),
+            child: Text(
+              'Files',
+              style: TextStyle(
+                fontSize: 25,
+                fontWeight: FontWeight.bold,
+                color: Colors.blue,
+              ),
+            ),
+          ),
+          Container(
+            height: 90,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: files.length,
+              itemBuilder: (BuildContext context, int index) {
+                return Container(
+                  width: 130,
+                  height: 50,
+                  margin:
+                      const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Center(
+                    child: Text(
+                      files[index],
+                      style: const TextStyle(fontSize: 18),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget bottomNav() {
+    return CircularBottomNavigation(
+      tabItems,
+      controller: _navigationController,
+      selectedPos: selectedPos,
+      barHeight: bottomNavBarHeight,
+      // use either barBackgroundColor or barBackgroundGradient to have a gradient on bar background
+      barBackgroundColor: Colors.white,
+      backgroundBoxShadow: const <BoxShadow>[
+        BoxShadow(color: Colors.black45, blurRadius: 10.0),
+      ],
+      animationDuration: const Duration(milliseconds: 300),
+      selectedCallback: (int? selectedPos) {
+        setState(() {
+          this.selectedPos = selectedPos ?? 0;
+          // print(_navigationController.value);
+        });
+      },
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _navigationController.dispose();
   }
 }
